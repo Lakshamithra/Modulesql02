b5617f77c2eb5dc759c23f704520fe01611a306c
Lakshamithra <143053825+Lakshamithra@users.noreply.github.com>
2025-05-27 21:01:19 +0530

    Create inactive.sql

---
+++ inactive.sql
@@ -0,0 +1,5 @@
+SELECT u.user_id, u.full_name
+FROM Users u
+LEFT JOIN Registrations r ON u.user_id = r.user_id
+WHERE r.registration_id IS NULL
+   OR r.registration_date < CURDATE() - INTERVAL 90 DAY;
